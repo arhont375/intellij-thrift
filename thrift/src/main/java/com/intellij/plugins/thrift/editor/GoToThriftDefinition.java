890768bb6c246dbece3eccbc016aa15820657ddd
haocheng <537312+haocheng@users.noreply.github.com>
2023-07-31 09:33:26 +0900

    Support Intellij IDEA 2023.2 (#14)
    * Support Intellij IDEA 2023.2
    
    * Upgrade versions in build.gradle/gradle.properties
    * Upgrade Gradle wrapper to 7.6.2 to fix the build failure
      caused by Jackson 2.15
    * Add junit-platform-launcher dependency for running tests
    
    * Fix 'Do not use PsiElement for popup model' error
    
    * Use PsiTargetNavigator instead to navigate or create popup
      for fixing the 'Do not use PsiElement for popup model' error
      in PopupFactoryImpl

--- thrift/src/main/java/com/intellij/plugins/thrift/editor/GoToThriftDefinition.java
+++ thrift/src/main/java/com/intellij/plugins/thrift/editor/GoToThriftDefinition.java
@@ -1,15 +1,14 @@
 package com.intellij.plugins.thrift.editor;
 
 import com.google.common.base.Stopwatch;
-import com.intellij.codeInsight.daemon.impl.PsiElementListNavigator;
-import com.intellij.ide.util.PsiElementListCellRenderer;
+import com.intellij.codeInsight.navigation.PsiTargetNavigator;
+import com.intellij.codeInsight.navigation.impl.PsiTargetPresentationRenderer;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.popup.JBPopup;
 import com.intellij.plugins.thrift.ThriftBundle;
 import com.intellij.plugins.thrift.index.ThriftDeclarationIndex;
 import com.intellij.plugins.thrift.lang.psi.ThriftDeclaration;
@@ -18,6 +17,7 @@ import com.intellij.psi.PsiClass;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
 import org.apache.commons.compress.utils.Lists;
+import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
@@ -26,6 +26,8 @@ import java.nio.file.Paths;
 import java.time.Duration;
 import java.util.List;
 
+import javax.swing.Icon;
+
 public class GoToThriftDefinition extends AnAction {
   static Logger logger = Logger.getInstance(GoToThriftDefinition.class);
 
@@ -33,53 +35,58 @@ public class GoToThriftDefinition extends AnAction {
   public void actionPerformed(@NotNull AnActionEvent event) {
     PsiElement psiElement = event.getData(CommonDataKeys.PSI_ELEMENT);
     List<ThriftDeclaration> thriftMatches = getThriftDeclarations(psiElement, event.getProject());
-    if (thriftMatches.size() > 0) {
-      PsiElementListCellRenderer<PsiElement> renderer = new PsiElementListCellRenderer<PsiElement>() {
-        @Override
-        public String getElementText(PsiElement element) {
-          if (element instanceof ThriftStruct) {
-            return ((ThriftStruct) element).getName();
-          } else {
-            return element.getText().substring(0, 20);
-          }
-        }
+    if (thriftMatches.size() == 1) {
+      // navigate directly if there's only 1 matched declaration
+      thriftMatches.get(0).navigate(true);
+    } else if (thriftMatches.size() > 1) {
+      final PsiTargetPresentationRenderer<ThriftDeclaration> presentationRenderer =
+              new PsiTargetPresentationRenderer<ThriftDeclaration>() {
+                @Nls
+                @Nullable
+                @Override
+                public String getContainerText(@NotNull ThriftDeclaration element) {
+                  Project project = event.getProject();
+                  Path containingFilePath = Paths.get(element.getContainingFile().getVirtualFile().getPath());
+                  if (project != null && project.getBasePath() != null) {
+                    Path projectBasePath = Paths.get(project.getBasePath()).normalize();
+                    return commonRoot(projectBasePath, containingFilePath).relativize(containingFilePath)
+                                                                          .toString();
+                  } else {
+                    return containingFilePath.toString();
+                  }
+                }
 
-        @NotNull
-        public Path commonRoot(@NotNull Path pathA, @NotNull Path pathB) {
-          Path rootCandidate = pathA;
-          while (rootCandidate.getParent() != null && rootCandidate.relativize(pathB).startsWith("..")) {
-            rootCandidate = rootCandidate.getParent();
-          }
-          return rootCandidate;
-        }
+                @NotNull
+                public Path commonRoot(@NotNull Path pathA, @NotNull Path pathB) {
+                  Path rootCandidate = pathA;
+                  while (rootCandidate.getParent() != null && rootCandidate.relativize(pathB).startsWith("..")) {
+                    rootCandidate = rootCandidate.getParent();
+                  }
+                  return rootCandidate;
+                }
 
-        @Override
-        protected @Nullable String getContainerText(PsiElement element, String name) {
-          Project project = event.getProject();
-          Path containingFilePath = Paths.get(element.getContainingFile().getVirtualFile().getPath());
-          if (project != null && project.getBasePath() != null) {
-            Path projectBasePath = Paths.get(project.getBasePath()).normalize();
-            return commonRoot(projectBasePath, containingFilePath).relativize(containingFilePath).toString();
-          } else {
-            return containingFilePath.toString();
-          }
-        }
+                @Nullable
+                @Override
+                protected Icon getIcon(@NotNull ThriftDeclaration element) {
+                  return super.getIcon(element);
+                }
 
-        @Override
-        protected int getIconFlags() {
-          return 0;
-        }
-      };
-      @Nullable JBPopup popup = PsiElementListNavigator.navigateOrCreatePopup(
-          thriftMatches.toArray(new ThriftDeclaration[0]),
-          ThriftBundle.message("thrift.goto.source"),
-          ThriftBundle.message("thrift.goto.source"),
-          renderer,
-          null
-      );
-      if (popup != null) {
-        popup.showInBestPositionFor(event.getDataContext());
-      }
+                @Nls
+                @NotNull
+                @Override
+                public String getElementText(@NotNull ThriftDeclaration element) {
+                  if (element instanceof ThriftStruct) {
+                    return element.getName();
+                  } else {
+                    return element.getText().substring(0, 20);
+                  }
+                }
+              };
+        // show popup if there are multiple matched declarations
+        new PsiTargetNavigator<>(thriftMatches)
+                .presentationProvider(presentationRenderer)
+                .createPopup(event.getProject(), ThriftBundle.message("thrift.goto.source"))
+                .showInBestPositionFor(event.getDataContext());
     }
   }
 
