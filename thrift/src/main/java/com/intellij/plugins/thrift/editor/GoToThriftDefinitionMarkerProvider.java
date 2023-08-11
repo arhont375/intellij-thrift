4fd044dff4df801ab4e32df39067f91ef7b6ece5
Cui-xf <1766788481@qq.com>
2023-08-11 16:14:34 +0800

    FIX:code style

--- thrift/src/main/java/com/intellij/plugins/thrift/editor/GoToThriftDefinitionMarkerProvider.java
+++ thrift/src/main/java/com/intellij/plugins/thrift/editor/GoToThriftDefinitionMarkerProvider.java
@@ -14,116 +14,126 @@ import com.intellij.plugins.thrift.ThriftBundle;
 import com.intellij.plugins.thrift.index.ThriftDeclarationIndex;
 import com.intellij.plugins.thrift.index.ThriftSubDeclarationIndex;
 import com.intellij.plugins.thrift.lang.psi.ThriftDeclaration;
-import com.intellij.psi.*;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiIdentifier;
+import com.intellij.psi.PsiMethod;
+import com.intellij.psi.PsiModifierList;
 import com.intellij.psi.impl.PsiSuperMethodImplUtil;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.util.PsiTreeUtil;
 import icons.ThriftIcons;
+import org.apache.commons.lang3.StringUtils;
 import org.jetbrains.annotations.NotNull;
 
 import java.time.Duration;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
 public class GoToThriftDefinitionMarkerProvider extends RelatedItemLineMarkerProvider {
-    static Logger logger = Logger.getInstance(GoToThriftDefinition.class);
+  static Logger logger = Logger.getInstance(GoToThriftDefinition.class);
 
-    private static Set<String> thriftStructBaseClasses =
-            Sets.newHashSet(
-                    "org.apache.thrift.TBase",
-                    "org.apache.thrift.TEnum",
-                    "com.twitter.scrooge.ThriftStruct",
-                    "com.twitter.scrooge.ThriftEnum",
-                    "com.twitter.scrooge.ThriftUnion",
-                    "com.twitter.scrooge.ThriftException"
-            );
+  private static Set<String> thriftStructBaseClasses =
+      Sets.newHashSet(
+          "org.apache.thrift.TBase",
+          "org.apache.thrift.TEnum",
+          "com.twitter.scrooge.ThriftStruct",
+          "com.twitter.scrooge.ThriftEnum",
+          "com.twitter.scrooge.ThriftUnion",
+          "com.twitter.scrooge.ThriftException"
+      );
 
-    private boolean isThriftStruct(PsiClass element) {
-        return Arrays
-                .stream(element.getSupers())
-                .anyMatch(superClass -> thriftStructBaseClasses.contains(superClass.getQualifiedName()))
-                || element.getAnnotation("javax.annotation.Generated") != null;
+  private boolean isThriftStruct(PsiClass element) {
+    return Arrays
+        .stream(element.getSupers())
+        .anyMatch(superClass -> thriftStructBaseClasses.contains(superClass.getQualifiedName()))
+        || element.getAnnotation("javax.annotation.Generated") != null;
+  }
+
+  @Override
+  protected void collectNavigationMarkers(@NotNull PsiElement element,
+                                          @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
+    List<ThriftDeclaration> thrift = findThriftDeclaration(element);
+    mark(element, result, thrift);
+  }
+
+  private List<ThriftDeclaration> findThriftDeclaration(@NotNull PsiElement element) {
+    if (element instanceof PsiClass && isThriftStruct((PsiClass) element)) {
+      PsiClass psiClass = (PsiClass) element;
+      return findByClass(psiClass);
+    } else if (element instanceof PsiMethod) {
+      PsiMethod method = (PsiMethod) element;
+      return findByMethod(method);
     }
+    return Collections.emptyList();
+  }
 
-    @Override
-    protected void collectNavigationMarkers(@NotNull PsiElement element,
-                                            @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result) {
-        List<ThriftDeclaration> thrift = findThriftDeclaration(element);
-        mark(element, result, thrift);
+  private List<ThriftDeclaration> findByMethod(PsiMethod method) {
+    String className = getClassName(method);
+    if (StringUtils.isEmpty(className)) {
+      return Collections.emptyList();
     }
+    String methodName = method.getName();
+    Stopwatch stopWatch = Stopwatch.createStarted();
+    List<ThriftDeclaration> thriftDefinitions =
+        ThriftSubDeclarationIndex.findDeclaration(className, methodName, method.getProject(),
+            GlobalSearchScope.allScope(method.getProject()));
+    Duration time = stopWatch.elapsed();
+    if (time.toMillis() > 20) {
+      logger.warn("GoToThriftDefinition action update took " + time.toMillis() + "ms");
+    }
+    return thriftDefinitions;
+  }
 
-    private List<ThriftDeclaration> findThriftDeclaration(@NotNull PsiElement element) {
-        if (element instanceof PsiClass && isThriftStruct((PsiClass) element)) {
-            PsiClass psiClass = (PsiClass) element;
-            return findByClass(psiClass);
-        } else if (element instanceof PsiMethod) {
-            PsiMethod method = (PsiMethod) element;
-            return findByMethod(method);
-        }
-        return Collections.emptyList();
-    }
+  private String getClassName(PsiMethod method) {
+    return Optional.ofNullable(PsiSuperMethodImplUtil.findDeepestSuperMethod(method))
+        .map(superMethod -> (PsiClass) PsiTreeUtil.findFirstParent(superMethod, psiElement -> {
+          if (psiElement instanceof PsiClass) {
+            return ((PsiClass) psiElement).hasAnnotation("javax.annotation.Generated");
+          }
+          return false;
+        }))
+        .map(NavigationItem::getName)
+        .orElse(null);
+  }
 
-    private List<ThriftDeclaration> findByMethod(PsiMethod method) {
-        String className = getClassName(method);
-        if (className != null) {
-            String methodName = method.getName();
-            Stopwatch stopWatch = Stopwatch.createStarted();
-            List<ThriftDeclaration> thriftDefinitions =
-                    ThriftSubDeclarationIndex.findDeclaration(className, methodName, method.getProject(),
-                            GlobalSearchScope.allScope(method.getProject()));
-            Duration time = stopWatch.elapsed();
-            if (time.toMillis() > 20) {
-                logger.warn("GoToThriftDefinition action update took " + time.toMillis() + "ms");
-            }
-            return thriftDefinitions;
-        }
-        return Collections.emptyList();
+  private List<ThriftDeclaration> findByClass(PsiClass psiClass) {
+    String name = psiClass.getName();
+    if (StringUtils.isEmpty(name)) {
+      return Collections.emptyList();
     }
+    Stopwatch stopWatch = Stopwatch.createStarted();
+    List<ThriftDeclaration> thriftDefinitions =
+        ThriftDeclarationIndex.findDeclaration(name, psiClass.getProject(),
+            GlobalSearchScope.allScope(psiClass.getProject()));
+    Duration time = stopWatch.elapsed();
+    if (time.toMillis() > 20) {
+      logger.warn("GoToThriftDefinition action update took " + time.toMillis() + "ms");
+    }
+    return thriftDefinitions;
+  }
 
-    private String getClassName(PsiMethod method) {
-        return Optional.ofNullable(PsiSuperMethodImplUtil.findDeepestSuperMethod(method))
-                .map(superMethod -> (PsiClass) PsiTreeUtil.findFirstParent(superMethod, psiElement -> {
-                    if (psiElement instanceof PsiClass) {
-                        return ((PsiClass) psiElement).hasAnnotation("javax.annotation.Generated");
-                    }
-                    return false;
-                }))
-                .map(NavigationItem::getName)
-                .orElse(null)
-                ;
+  private void mark(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result, List<ThriftDeclaration> thriftDefinitions) {
+    if (thriftDefinitions.isEmpty()) {
+      return;
     }
-
-    private List<ThriftDeclaration> findByClass(PsiClass psiClass) {
-        String name = psiClass.getName();
-        if (name != null) {
-            Stopwatch stopWatch = Stopwatch.createStarted();
-            List<ThriftDeclaration> thriftDefinitions =
-                    ThriftDeclarationIndex.findDeclaration(name, psiClass.getProject(),
-                            GlobalSearchScope.allScope(psiClass.getProject()));
-            Duration time = stopWatch.elapsed();
-            if (time.toMillis() > 20) {
-                logger.warn("GoToThriftDefinition action update took " + time.toMillis() + "ms");
-            }
-            return thriftDefinitions;
-        }
-        return Collections.emptyList();
+    NavigationGutterIconBuilder<PsiElement> builder =
+        NavigationGutterIconBuilder.create(ThriftIcons.STRUCT)
+            .setTargets(thriftDefinitions)
+            .setTooltipText(ThriftBundle.message("thrift.goto.source"));
+    Optional<PsiElement> identifier =
+        Arrays.stream(element.getChildren()).filter(x -> x instanceof PsiIdentifier).findFirst();
+    // Some languages (Scala) don't put PsiIdentifier here, PsiModifierList is a fallback
+    Optional<PsiElement> modifiersList =
+        Arrays.stream(element.getChildren()).filter(x -> x instanceof PsiModifierList).findFirst();
+    if (identifier.isPresent()) {
+      result.add(builder.createLineMarkerInfo(identifier.get()));
+    } else if (modifiersList.isPresent()) {
+      result.add(builder.createLineMarkerInfo(modifiersList.get()));
     }
-
-    private void mark(@NotNull PsiElement element, @NotNull Collection<? super RelatedItemLineMarkerInfo<?>> result, List<ThriftDeclaration> thriftDefinitions) {
-        if (!thriftDefinitions.isEmpty()) {
-            NavigationGutterIconBuilder<PsiElement> builder =
-                    NavigationGutterIconBuilder.create(ThriftIcons.STRUCT)
-                            .setTargets(thriftDefinitions)
-                            .setTooltipText(ThriftBundle.message("thrift.goto.source"));
-            Optional<PsiElement> identifier =
-                    Arrays.stream(element.getChildren()).filter(x -> x instanceof PsiIdentifier).findFirst();
-            // Some languages (Scala) don't put PsiIdentifier here, PsiModifierList is a fallback
-            Optional<PsiElement> modifiersList =
-                    Arrays.stream(element.getChildren()).filter(x -> x instanceof PsiModifierList).findFirst();
-            if (identifier.isPresent()) {
-                result.add(builder.createLineMarkerInfo(identifier.get()));
-            } else if (modifiersList.isPresent()) {
-                result.add(builder.createLineMarkerInfo(modifiersList.get()));
-            }
-        }
-    }
+  }
 }
