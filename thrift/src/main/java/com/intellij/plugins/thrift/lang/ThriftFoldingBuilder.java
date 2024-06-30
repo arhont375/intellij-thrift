1fa114727ad5ab6e196d5f7d25aee0e1bf6f3873
CxdInitial <2caoxingding@gmail.com>
2024-06-30 20:29:17 +0800

    feat: add folding builder (#34)
    * feat: add folding builder
    
    * refactor(folding): remove @Test annotation and unnecessary visitor method
    
    * Update thrift/src/test/resources/folding/foldingTwice.thrift
    
    ---------
    
    Co-authored-by: CxdInitial <CxdInitial@users.noreply.github.com>
    Co-authored-by: arhont375 <arhont375@users.noreply.github.com>

---
+++ thrift/src/main/java/com/intellij/plugins/thrift/lang/ThriftFoldingBuilder.java
@@ -0,0 +1,146 @@
+package com.intellij.plugins.thrift.lang;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.lang.folding.FoldingBuilderEx;
+import com.intellij.lang.folding.FoldingDescriptor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.project.DumbAware;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.plugins.thrift.lang.lexer.ThriftElementType;
+import com.intellij.psi.JavaRecursiveElementWalkingVisitor;
+import com.intellij.psi.PsiElement;
+import com.intellij.plugins.thrift.lang.psi.*;
+import com.intellij.psi.tree.IElementType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.intellij.plugins.thrift.lang.lexer.ThriftTokenTypes.*;
+
+
+public class ThriftFoldingBuilder extends FoldingBuilderEx implements DumbAware {
+  @Override
+  public FoldingDescriptor @NotNull [] buildFoldRegions(@NotNull PsiElement root, @NotNull Document document, boolean quick) {
+    List<FoldingDescriptor> descriptors = new ArrayList<>();
+    root.accept(new ThriftFoldingRecursiveElementWalkingVisitor(descriptors));
+    return descriptors.toArray(FoldingDescriptor.EMPTY_ARRAY);
+  }
+
+  @Override
+  public @Nullable String getPlaceholderText(@NotNull ASTNode node) {
+    PsiElement psiElement = node.getPsi();
+    String placeholderText = "...";
+    if (psiElement instanceof ThriftEnum) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftSenum) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftStruct) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftUnion) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftException) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftService) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftXsdAttrs) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftConstMap) {
+      placeholderText = "{...}";
+    } else if (psiElement instanceof ThriftConstList) {
+      placeholderText = "[...]";
+    } else if (isOfThriftElementType(node.getElementType(), BLOCKCOMMENT)) {
+      placeholderText = "/*...*/";
+    }
+    return placeholderText;
+  }
+
+  @Override
+  public boolean isCollapsedByDefault(@NotNull ASTNode node) {
+    return false;
+  }
+
+  static boolean isOfThriftElementType(IElementType elementType, IElementType thriftElementType) {
+    if (elementType instanceof ThriftElementType) {
+      return elementType.equals(thriftElementType);
+    }
+    return false;
+  }
+
+  static class ThriftFoldingRecursiveElementWalkingVisitor extends JavaRecursiveElementWalkingVisitor {
+    private final List<FoldingDescriptor> descriptors;
+
+    public ThriftFoldingRecursiveElementWalkingVisitor(List<FoldingDescriptor> descriptors) {
+      this.descriptors = descriptors;
+    }
+
+    @Override
+    public void visitElement(@NotNull PsiElement element) {
+      super.visitElement(element);
+
+      if (element instanceof ThriftEnum) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftSenum) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftStruct) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftUnion) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftException) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftService) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftXsdAttrs) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftConstMap) {
+        this.visitElementWithCurlyBraceChildren(element);
+      } else if (element instanceof ThriftConstList) {
+        this.visitThriftConstList((ThriftConstList) element);
+      } else if (isOfThriftElementType(element.getNode().getElementType(), BLOCKCOMMENT)) {
+        this.visitThriftBlockComment(element.getNode());
+      }
+    }
+
+    private void visitElementWithCurlyBraceChildren(PsiElement element) {
+      ASTNode[] children = element.getNode().getChildren(null);
+      int beg, end;
+      for (beg = 0; beg < children.length; beg++) {
+        if (isOfThriftElementType(children[beg].getElementType(), LEFTCURLYBRACE)) {
+          break;
+        }
+      }
+      for (end = children.length - 1; end >= 0; end--) {
+        if (isOfThriftElementType(children[end].getElementType(), RIGHTCURLYBRACE)) {
+          break;
+        }
+      }
+      if (beg < children.length && end >= 0) {
+        this.descriptors.add(new FoldingDescriptor(element, new TextRange(children[beg].getStartOffset(), children[end].getStartOffset() + 1)));
+      }
+    }
+
+    private void visitThriftConstList(ThriftConstList element) {
+      ASTNode[] children = element.getNode().getChildren(null);
+      int beg, end;
+      for (beg = 0; beg < children.length; beg++) {
+        if (isOfThriftElementType(children[beg].getElementType(), LEFTBRACKET)) {
+          break;
+        }
+      }
+      for (end = children.length - 1; end >= 0; end--) {
+        if (isOfThriftElementType(children[end].getElementType(), RIGHTBRACKET)) {
+          break;
+        }
+      }
+      if (beg < children.length && end >= 0) {
+        this.descriptors.add(new FoldingDescriptor(element, new TextRange(children[beg].getStartOffset(), children[end].getStartOffset() + 1)));
+      }
+    }
+
+    private void visitThriftBlockComment(ASTNode node) {
+      this.descriptors.add(new FoldingDescriptor(node,
+          new TextRange(node.getStartOffset(), node.getStartOffset() + node.getTextLength())));
+    }
+  }
+}
---
+++ thrift/src/test/java/com/intellij/plugins/thrift/folding/ThriftFoldingBuilderTest.java
@@ -0,0 +1,26 @@
+package com.intellij.plugins.thrift.folding;
+
+import com.intellij.plugins.thrift.ThriftCodeInsightFixtureTestCase;
+import org.junit.jupiter.api.Test;
+
+public class ThriftFoldingBuilderTest extends ThriftCodeInsightFixtureTestCase {
+  @Override
+  protected String getBasePath() {
+    return "folding";
+  }
+
+  protected void doTest() throws Throwable {
+    myFixture.configureByFiles();
+    String file = getTestName(true) + ".thrift";
+    myFixture.configureByFile(file);
+    myFixture.testFolding(getTestDataPath() + "/" + file);
+  }
+
+  public void testFoldingOnce() throws Throwable {
+    doTest();
+  }
+
+  public void testFoldingTwice() throws Throwable {
+    doTest();
+  }
+}
