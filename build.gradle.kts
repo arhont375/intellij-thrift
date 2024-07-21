87052d9b47cfce041900b871101149854b3e06e5
arhont375 <arhont375@users.noreply.github.com>
2024-07-21 23:44:55 +0900

    Plugin 2.0.0-RC1

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    id("org.jetbrains.intellij.platform") version "2.0.0-beta9" apply false
+    id("org.jetbrains.intellij.platform") version "2.0.0-RC1" apply false
     id("java")
     id("idea")
 }
